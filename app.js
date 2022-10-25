c38cf4cf727d00c7ca2c1108cb6842e7b4eb7057
waikoo <b4rn1k4@yahoo.com>
2022-10-26 00:54:12 +0300

    Fix end game UI errors

--- app.js
+++ app.js
@@ -5,43 +5,48 @@ import getRandomNumberUntil from './utils/randomNumberGenerator.js';
 // ! name
 function handleNameSubmission(e) {
 	e.preventDefault();
-	const playerName = $('#name').value;
-	playerName ? setPlayerName(playerName) : setPlayerName();
+	lightUpLogo('paper');
+	animate('out', 'name');
 
-	$('#form-name').classList.add('go-out');
+	setPlayerName();
+
 	setTimeout(() => {
 		makeInvisible($('#form-name'));
 		makeVisible($('#form-color'));
-	}, 300);
-
-	$('#paper').classList.add('rps-animation');
-	$('#paper').classList.remove('unlit');
+		animate('in', 'color');
+	}, 100);
 }
 function setPlayerName(name = 'Player') {
-	game.name = name.trim();
+	$('#name').value ? (game.name = $('#name').value.trim()) : (game.name = name);
 	$('.player-name').textContent = game.name;
 }
 // ! color
 function handleColorSubmission(e) {
 	e.preventDefault();
-	if (game.playerColor) {
-		game.computerColor = getRandomColor();
-	} else {
-		game.playerColor = getRandomColor();
-	}
 
 	animateUIAtColorStage();
-	loadColoredRPS();
+
+	setTimeout(() => {
+		makeVisible($('.play-page'));
+		makeInvisible($('main'));
+	}, 100);
+
+	setTimeout(() => {
+		$('#form-color').removeEventListener('submit', handleColorSubmission);
+		$('#form-color').style.zIndex = '-100';
+	}, 3000);
 }
 function handleColorSelection(e) {
-	colors.forEach(color => {
+	$$('.color-item-first').forEach(color => {
 		if (color === e.target) {
 			color.classList.add('selected');
 			game.playerColor = e.target.dataset.color;
+			game.computerColor = getRandomColor();
 		} else {
 			color.classList.remove('selected');
 		}
 	});
+	console.log(game.playerColor);
 }
 function getRandomColor() {
 	const randomColorObj = {
@@ -54,8 +59,16 @@ function getRandomColor() {
 	return randomColorObj[`${getRandomNumberUntil(5)}`];
 }
 function loadColoredRPS() {
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! TO REFACTOR
+	if (game.playerColor) {
+		game.computerColor = getRandomColor();
+	} else {
+		game.playerColor = getRandomColor();
+	}
+
 	if (!game.computerColor) game.computerColor = getRandomColor();
-	if (game.playerColor === game.computerColor) game.computerColor = getRandomColor();
+	while (game.playerColor === game.computerColor) game.computerColor = getRandomColor();
+	// if (game.playerColor === game.computerColor) game.computerColor = getRandomColor();
 	// TODO: avoid same color being used. here.
 
 	$('.computer-rock').src = `./images/colored/rock-${game.computerColor}.png`;
@@ -66,16 +79,19 @@ function loadColoredRPS() {
 	$('.paper-img').src = `./images/colored/paper-${game.playerColor}.png`;
 	$('.scissors-img').src = `./images/colored/scissors-${game.playerColor}.png`;
 }
+function lightUpLogo(rps) {
+	$(`#${rps}`).classList.add('rps-animation');
+	$(`#${rps}`).classList.remove('unlit');
+}
 function animateUIAtColorStage() {
-	$('#scissors').classList.add('rps-animation');
-	$('#scissors').classList.remove('unlit');
-
-	$('#form-color').classList.add('go-out');
-	setTimeout(() => {
-		makeInvisible($('#form-color'));
-		makeInvisible($('main'));
-		makeVisible($('.play-page'));
-	}, 300);
+	animate('out', 'color');
+	loadColoredRPS();
+	lightUpLogo('scissors');
+	//
+	// !$('.player-history-item-1 img').src ? makeInvisible($('main')) : makeVisible($('main'));
+	// if smth breaks, these:
+	// $('#form-color').classList.remove('disappear');
+	// makeInvisible('#form-color');
 }
 // ! play
 function getComputerChoice() {
@@ -86,7 +102,6 @@ function getComputerChoice() {
 	};
 	return numberToChoiceObj[`${getRandomNumberUntil(3)}`];
 }
-
 function incrementScoreFor(aPlayer = 'both') {
 	if (aPlayer === 'both') {
 		incrementScoreFor('computer');
@@ -97,34 +112,45 @@ function incrementScoreFor(aPlayer = 'both') {
 	}
 }
 
+function darkenPlayArea() {
+	$('.mid-con').classList.add('low-opacity');
+	$$('.block').forEach(item => item.classList.add('low-opacity'));
+}
+function lightenPlayArea() {
+	$('.mid-con').classList.remove('low-opacity');
+	$$('.block').forEach(item => item.classList.remove('low-opacity'));
+}
 function endGameUI() {
-	// $('.play-page').style.opacity = '.2';
-	// $('.endgame-settings').style.opacity = '1';
-	$('.endgame-settings').classList.add('endgame-settings-end');
-	$('.play-page').classList.add('play-page-end');
-	document.body.classList.add('body-end');
-	$('main').classList.add('main-end');
-	// document.body.style.display = 'relative';
-	// $('main').style.position = 'absolute';
-	// $('main').style.top = 0;
-	// $('main').style.bottom = 0;
-	// $('main').style.left = 0;
-	// $('main').style.right = 0;
-	// $('main').style.height = '100vh';
+	darkenPlayArea();
+	$('.endgame-con').style.display = 'flex';
+  // animateEndgame('in')
+  $('.endgame-settings').style.animation = `100ms come-in forwards`
+  // $('.endgame-settings').style.animation = `100ms come-${inOrOut} forwards`
 }
+function displayWinner() {
+	const playerScore = Number($('.player-score').textContent);
+	const computerScore = Number($('.computer-score').textContent);
+	let scoreboard = $('.endgame-message').textContent;
 
-function displayMessage() {
-	Number($('.player-score').textContent) > Number($('.computer-score').textContent) ? ($('.endgame-message').textContent = 'YOU WIN') : ($('.endgame-message').textContent = 'YOU LOSE');
-	if (Number($('.player-score').textContent) === Number($('.computer-score').textContent)) {
-		$('.endgame-message').textContent = "IT'S A TIE";
-	}
+	playerScore > computerScore ? (scoreboard = 'YOU WIN') : (scoreboard = 'YOU LOSE');
+	if (playerScore === computerScore) scoreboard = "IT'S A TIE";
 }
-
+function createEndgameContainer() {
+	const endgameCon = document.createElement('div');
+	endgameCon.className = 'endgame-con';
+	endgameCon.append($('.endgame-settings'));
+	$('.play-page').append(endgameCon);
+}
 function showSettings() {
+	// if (game.isFirstGame) game.isFirstGame = false;
+	if (!$('.play-page').contains($('.endgame-con'))) createEndgameContainer();
+	$('.endgame-con').removeAttribute('style');
 	makeVisible($('.endgame-settings'));
-	makeVisible($('main'));
-	displayMessage();
-
+  // console.log($('.endgame-settings').style.animation)
+  animateEndgame('in')
+  // setTimeout(() => $('.endgame-settings').removeAttribute('style'), 100)
+  // removeAttribute?
+	displayWinner();
 	endGameUI();
 }
 
@@ -203,14 +229,7 @@ function Round(playerChoice) {
 	};
 
 	this.showWinner = function (player, rps) {
-		console.warn('before:');
-		console.log($('.player-history-item-1'));
-		console.log($('.player-history-item-1 img').src);
-		console.log(game.round);
 		$(`.${player}-history-item-${game.round} img`).setAttribute('src', `./images/rock-paper-scissor/${rps}-winner.svg`);
-
-		console.warn('after');
-		console.log($('.player-history-item-1 img').src);
 	};
 	this.showLoser = function (player, rps) {
 		$(`.${player}-history-item-${game.round} img`).setAttribute('src', `./images/rock-paper-scissor/${rps}-loser.svg`);
@@ -253,15 +272,15 @@ const game = {
 	computerColor: null,
 	computerScoreValue: 0,
 	playerScoreValue: 0,
-	round: 1
+	round: 1,
+	isFirstGame: true,
+	isColorChanged: false
 };
-
-const colors = [...$('.color-con').children];
 
 $('.refresh').addEventListener('click', handleRefresh);
 $('#form-name').addEventListener('submit', handleNameSubmission);
 $('#form-color').addEventListener('submit', handleColorSubmission);
-colors.forEach(color => color.addEventListener('click', handleColorSelection));
+$$('.color-item-first').forEach(color => color.addEventListener('click', handleColorSelection));
 $('.refresh').addEventListener('mouseenter', addLogoGlow, true);
 $('.refresh').addEventListener('mouseleave', removeLogoGlow, true);
 
@@ -271,20 +290,47 @@ $('.play-again').addEventListener('click', handlePlayAgain);
 $('.change-name').addEventListener('click', handleChangeName);
 $('.change-color').addEventListener('click', handleChangeColor);
 
-function handlePlayAgain() {
-	makeInvisible($('main'));
-	makeInvisible($('.endgame-settings'));
-	resetUI();
+function resetUI() {
+	resetParts();
 	resetScore();
 	resetHistory();
 	resetComputerColor();
 }
 
-function resetUI() {
-	$('.play-page').classList.remove('play-page-end');
+function resetPlayerName() {
+	let newName = $('#name-change').value;
+	game.name = newName ? newName : 'PLAYER';
+	$('.player-name').textContent = game.name;
+}
+
+function resetPlayerColor() {
+	makeInvisible($('.change-color-con'));
+	$('.change-color-con').style.display = 'none';
+}
+
+function handlePlayAgain(e) {
+	e.preventDefault();
+	if (e.target === $('.play-btn-name')) resetPlayerName();
+	if (e.target === $('.play-btn-color')) resetPlayerColor();
+	game.isFirstGame = false;
+	resetUI();
+}
+function resetParts() {
+	// $('.play-page').classList.remove('play-page-end');
 	document.body.classList.remove('body-end');
 	$('main').classList.remove('main-end');
 	$('.endgame-settings').classList.remove('endgame-settings-end');
+
+	lightenPlayArea();
+	makeInvisible($('main'));
+	makeInvisible($('.endgame-settings'));
+	if ($('.change-name-con')) {
+		makeInvisible($('.change-name-con'));
+		$('.change-name-con').style.display = 'none';
+	}
+	$('.endgame-con').style.display = 'none';
+	makeInvisible($('.endgame-con'));
+	$('.endgame-con').style.zIndex = '-5';
 }
 
 function resetScore() {
@@ -299,92 +345,249 @@ function resetScore() {
 
 function resetHistory() {
 	for (let n = 1; n < 6; n++) {
-		// $(`.player-history-item-${n} img`).src = '';
-		// $(`.computer-history-item-${n} img`).src = '';
 		$(`.player-history-item-${n} img`).removeAttribute('src');
 		$(`.computer-history-item-${n} img`).removeAttribute('src');
 	}
 }
 
 function resetComputerColor() {
-	// TODO: refactor into getNewComputerColor()
+	getNewComputerColor();
+	loadColoredRPS();
+}
+
+function getNewComputerColor() {
 	const oldColor = game.computerColor;
 	let newColor = getRandomColor();
 	while (newColor === oldColor) {
 		newColor = getRandomColor();
 	}
 	game.computerColor = newColor;
-	// TODO: ^
-	loadColoredRPS();
+}
+
+function generateNameChangeHTML() {
+	const nameDiv = document.createElement('div');
+	nameDiv.className = 'change-name-con';
+
+	$('.play-page').append(nameDiv);
+	nameDiv.innerHTML = /*html*/ `
+  <form
+    class="input-form"
+  >
+    <label
+      for="name-change"
+      class="input-label"
+    >Enter your name
+    </label>
+    <div class="name-input-text">
+      <div class="input-small-con">
+        <input
+          type="text"
+          name="name-change"
+          id="name-change"
+          maxlength="8"
+          placeholder="PLAYER"
+          spellcheck="false"
+        >
+        <small>Maximum 8 Characters</small>
+      </div>
+      <div class="change-btn-con">
+        <button class="back-btn-name btn">Back</button>
+        <button
+          class="btn play-btn-name">
+          Play
+        </button>
+      </div>
+    </div>
+  </form>`;
+	$('.back-btn-name').addEventListener('click', handleGoBack);
+	$('.play-btn-name').addEventListener('click', handlePlayAgain);
 }
 
 function handleChangeName() {
-	const playWithNewNameButton = createPlayButton();
-	$('#form-name button').replaceWith(playWithNewNameButton);
+  animateEndgame('out')
+  setTimeout(() => {
+    makeInvisible($('main'));
+    makeInvisible($('.endgame-settings'));
+    $('.endgame-con').style.display = 'none';
+    $('.endgame-settings').style.animation = '';
+  }, 100)
 
-	animateChoose('name');
+	if (!checkParent($('.play-page'), $('.change-name-con'))) {
+		generateNameChangeHTML();
+	} else {
+		$('.change-name-con').style.display = 'flex';
+		makeVisible($('.change-name-con'));
+	}
+
+	if ($('.play-page').lastChild !== $('.change-name-con')) {
+		$('.play-page').append($('.change-name-con'));
+	}
+  animateChange('in', 'name')
+  animateEndgame('out')
 }
+
 function handleChangeColor() {
-	const playWithNewColorButton = createPlayButton();
-	$('#form-color button').replaceWith(playWithNewColorButton);
+  animateEndgame('out')
+  setTimeout(() => {
+    makeInvisible($('main'));
+    makeInvisible($('.endgame-settings'));
+    $('.endgame-con').style.display = 'none';
+  })
 
-	animateChoose('color');
+	game.isColorChanged = true;
+
+	if (!checkParent($('.play-page'), $('.change-color-con'))) {
+		generateColorChangeHTML();
+	} else {
+		$('.change-color-con').style.display = 'flex';
+		makeVisible($('.change-color-con'));
+	}
+
+	[...$$('.change-color-item')].forEach(item => {
+		item.addEventListener('click', duplicateHandleColorSelection);
+	});
+
+	loadColoredRPS();
+  animateChange('in', 'name')
 }
 
-function createPlayButton() {
-	const playButton = document.createElement('button');
-	playButton.classList.add('btn');
-	playButton.addEventListener('click', handlePlayAgain);
-	playButton.textContent = 'Play';
-	return playButton;
+function checkParent(parent, child) {
+	if (parent.contains(child)) return true;
+	return false;
 }
 
-function animateChoose(nameOrColor) {
-	makeInvisible($('.endgame-settings'));
-	$('.play-page').style.zIndex = '-1';
-
-	$(`#form-${nameOrColor}`).classList.remove('go-out');
-	$(`#form-${nameOrColor}`).style.position = 'relative';
-	$(`#form-${nameOrColor}`).style.zIndex = '200';
-	makeVisible($(`#form-${nameOrColor}`));
+function duplicateHandleColorSelection(e) {
+	$$('.change-color-item').forEach(color => {
+		if (color === e.target) {
+			color.classList.add('selected');
+			game.playerColor = e.target.dataset.color;
+			game.computerColor = getRandomColor();
+		} else {
+			color.classList.remove('selected');
+		}
+	});
 }
 
-// function checkEndGame() {
-// 	// playRoundWithIf(playerChoice, getComputerChoice());
-// 	console.log('endGame ran');
-// 	if (playerScore.textContent === '5') {
-// 		playOutcome.textContent = 'Congratulations! You won!';
-// 		playOutcome.style.fontSize = '2rem';
-// 		playOutcome.style.fontWeight = '800';
-// 		playOutcome.style.color = 'green';
-// 		playerScoreValue = 0;
-// 		computerScoreValue = 0;
-// 		playerScore.textContent = 0;
-// 		computerScore.textContent = 0;
-// 		clearInterval(intervalId);
-// 		playNewGame.style.display = 'block';
-// 		playNewGame.addEventListener('click', () => {
-// 			startGame();
-// 			playerScore.textContent = 0;
-// 			computerScore.textContent = 0;
-// 			playerScoreValue = 0;
-// 			computerScoreValue = 0;
-// 			playOutcome.textContent = '';
-// 			playOutcome.style.color = 'black';
-// 		});
-// 	} else if (computerScore.textContent === '5') {
-// 		playOutcome.textContent = 'You lose! Try again.';
-// 		playOutcome.style.color = 'red';
-// 		clearInterval(intervalId);
-// 		playNewGame.style.display = 'block';
-// 		playNewGame.addEventListener('click', () => {
-// 			startGame();
-// 			playerScore.textContent = 0;
-// 			computerScore.textContent = 0;
-// 			playerScoreValue = 0;
-// 			computerScoreValue = 0;
-// 			playOutcome.textContent = '';
-// 			playOutcome.style.color = 'black';
-// 		});
-// 	}
-// }
+function generateColorChangeHTML() {
+	const nameDiv = document.createElement('div');
+	nameDiv.className = 'change-color-con';
+
+	$('.play-page').append(nameDiv);
+	nameDiv.innerHTML = /*html*/ `
+  <form
+      id="color-change"
+      class="input-form"
+    >
+      <label
+        for="color"
+        class="input-label"
+      >Choose Your Color</label>
+      <div class="color-btn-con">
+        <div class="color-con">
+          <img
+            src="images/player-color/red.png"
+            alt="red"
+            data-color="red"
+            class="change-color-item"
+          >
+          <img
+            src="images/player-color/yellow.png"
+            alt="yellow"
+            data-color="yellow"
+            class="change-color-item"
+          >
+          <img
+            src="images/player-color/purple.png"
+            alt="purple"
+            data-color="purple"
+            class="change-color-item"
+          >
+          <img
+            src="images/player-color/green.png"
+            alt="green"
+            data-color="green"
+            class="change-color-item"
+          >
+          <img
+            src="images/player-color/blue.png"
+            alt="blue"
+            data-color="blue"
+            class="change-color-item"
+          >
+        </div>
+        <div class="change-btn-con">
+          <button class="back-btn-color btn">Back</button>
+          <button
+            id="color-btn"
+            class="btn play-btn-color"
+          >Play</button>
+      </div>
+    </form>
+  `;
+
+	$('.back-btn-color').addEventListener('click', handleGoBack);
+	$('.play-btn-color').addEventListener('click', handlePlayAgain);
+}
+function handleGoBack(e) {
+	e.preventDefault();
+
+	game.isColorChanged = false;
+
+	if (checkParent($('.play-page'), $('.change-name-con'))) {
+    animateChange('out', 'name')
+    animateEndgame('in-left')
+    // ($('.change-name-con').style.display = 'none')
+  }
+  if (checkParent($('.play-page'), $('.change-color-con'))) {
+    ($('.change-color-con').style.display = 'none');
+  } 
+
+  setTimeout(() => { 
+    handleGoBackUI();
+  }, 100)
+  // animateEndgame('in', 'reverse')
+}
+
+function handleGoBackUI() {
+	makeVisible($('.endgame-con'));
+	$('.endgame-con').style.display = 'flex';
+
+	makeVisible($('.endgame-settings')); // no work
+  $('.endgame-settings').style.animation = '';
+	$('.endgame-settings').style.display = 'block';
+  $('.change-name-con').style.animation = '100ms come-in-left forwards 1'
+  // $('.endgame-settings').style.animation = '100ms come-in-left forwards 1'
+  setTimeout(() => {
+    $('.change-name-con').style.display = 'none'
+  }, 100)
+}
+
+function animate(inOrOut, nameOrColor) {
+	let form = $(`#form-${nameOrColor}`);
+	if (inOrOut === 'in') form.style.animation = '100ms come-in none';
+	if (inOrOut === 'out') form.style.animation = '100ms go-out none';
+}
+
+function animateChange(inOrOut, nameOrColor) {
+  console.log('goback triggered name to go right')
+	let con = $(`.change-${nameOrColor}-con`);
+	if (inOrOut === 'in') {
+    con.style.animation = '100ms come-in none';
+    $('.change-name-con').style.animation = '100ms go-out none'
+  }
+	if (inOrOut === 'out') con.style.animation = '100ms go-out none 1';
+}
+
+function animateEndgame(inOrOut) {
+  // const endgame = $('.endgame-settings').style
+  // inOrOut === 'in'
+  //         ? endgame.animation = `100ms come-in ${reverse} 1`
+	 //        : endgame.animation = `100ms go-out ${reverse} 1`;
+  if (inOrOut === 'out') $('.endgame-settings').style.animation = `100ms go-out none 1`
+  if (inOrOut === 'in') $('.endgame-settings').style.animation = `100ms come-in none 1`
+  if (inOrOut === 'in-left') {
+    $('.endgame-settings').style.animation = `100ms come-in-left none 1`
+  }
+  console.log()
+}
+
